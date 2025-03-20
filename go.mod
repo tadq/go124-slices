b6728cfb734f62568fbad5760f3c6890e6106628
tadq <tadvizbaras@gmail.com>
2025-03-20 15:20:09 -0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tadq/go124-slices
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	iterTry()
+}
+
+func getAll() {
+	vals := []string{"one", "two", "three"}
+
+	for i, v := range slices.All(vals) {
+		fmt.Println(i, v)
+	}
+
+	for i, v := range slices.Backward(vals) {
+		fmt.Println(i, v)
+	}
+}
+
+func appendSeq() {
+	seq := func(yield func(int) bool) {
+		for i := range 4 {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+
+	s := slices.AppendSeq([]int{1, 2, 3}, seq)
+	fmt.Println(s)
+}
+
+func makeSlice() []string {
+	return []string{"one", "two", "three", "four"}
+}
+
+// Perform binary search.
+func binarySearch() {
+	s := slices.Clone(makeSlice())
+	slices.Sort(s)
+	n, found := slices.BinarySearch(s, "four")
+	fmt.Println(n, found)
+	nn, nfound := slices.BinarySearch(s, "zero")
+	fmt.Println(nn, nfound)
+}
+
+func clip() {
+	a := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s := a[:4:10]
+	fmt.Println(cap(s))
+	clip := slices.Clip(s)
+	fmt.Println(cap(clip))
+	fmt.Println(clip)
+}
+
+func iterTry() {
+	seq := func(yield func(i int) bool) {
+		for i := range 8 {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+
+	for n := range seq {
+		fmt.Println(n)
+	}
+}
